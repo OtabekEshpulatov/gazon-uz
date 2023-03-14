4af1617ac489881b15ba85f0fa3d9f1726ec1962
nodir <himmatovnodir1512@gmail.com>
2023-03-14 15:59:12 +0500

    ishni boshladik v3

--- src/main/java/com/noobs/gazonuz/controllers/PitchPostController.java
+++ src/main/java/com/noobs/gazonuz/controllers/PitchPostController.java
@@ -1,5 +1,23 @@
 package com.noobs.gazonuz.controllers;
 
-public class PitchPostController {
+import com.noobs.gazonuz.domains.Pitch;
+import com.noobs.gazonuz.repositories.PitchRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping( "/pithInfo/" )
+@RequiredArgsConstructor
+public class PitchPostController {
+    @GetMapping("/pitchInfo/{id}")
+    public String showFormForUpdate(@PathVariable( value = "id") long id, Model model) {
+
+        Pitch pitch = PitchRepo.(id);
+        model.addAttribute("pitch", pitch);
+        return "update_course";
+    }
 }
