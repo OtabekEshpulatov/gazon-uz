9b09ede358a66cbb95b33a27bcf9a54d8878c331
otabek <otabekeshpulatov123@gmail.com>
2023-03-14 15:58:11 +0500

    full functionality of authentication

--- src/main/java/com/noobs/gazonuz/controllers/AuthController.java
+++ src/main/java/com/noobs/gazonuz/controllers/AuthController.java
@@ -10,10 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -26,9 +23,15 @@ public class AuthController {
 
 
     @GetMapping( "/login" )
-    public String getLogin() {
-        return "auth/login";
+    public ModelAndView getLogin(@RequestParam( name = "error", required = false ) String error) {
+        var mav = new ModelAndView();
+        mav.addObject("error" , error);
+        mav.setViewName("auth/login");
+        return mav;
     }
+
+
+
 
 
     @GetMapping( "/register" )
