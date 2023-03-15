f135711ed54885bd4570c47f105641f7fa9b6712
nodir <himmatovnodir1512@gmail.com>
2023-03-15 14:52:18 +0500

    404 page not found added

--- src/main/java/com/noobs/gazonuz/controllers/PitchPostController.java
+++ src/main/java/com/noobs/gazonuz/controllers/PitchPostController.java
@@ -1,23 +1,36 @@
 package com.noobs.gazonuz.controllers;
 
 import com.noobs.gazonuz.domains.Pitch;
+import com.noobs.gazonuz.handler.CustomRuntimeException;
 import com.noobs.gazonuz.repositories.PitchRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping( "/pithInfo/" )
+import java.util.Optional;
+
+@Controller
+@RequestMapping( "/pitchInfo" )
 @RequiredArgsConstructor
 public class PitchPostController {
-    @GetMapping("/pitchInfo/{id}")
-    public String showFormForUpdate(@PathVariable( value = "id") long id, Model model) {
 
-        Pitch pitch = PitchRepo.(id);
-        model.addAttribute("pitch", pitch);
-        return "update_course";
+
+    private final PitchRepo pitchRepo;
+    @GetMapping("/{id}")
+    public String showPitchInfo(@PathVariable( value = "id") String id, Model model) {
+
+        Optional<Pitch> pitch = pitchRepo.findById(id);
+        if(pitch.isPresent()){
+            model.addAttribute("pitch", pitch);
+            return "pitch/PitchMainForCopy";
+        }else{
+            throw new CustomRuntimeException("Page not found");
+        }
+
     }
 }
