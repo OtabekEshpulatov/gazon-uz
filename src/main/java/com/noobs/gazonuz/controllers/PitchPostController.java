f952c63325e24e4d92ede27230b564e1f3e7ea69
otabek <otabekeshpulatov123@gmail.com>
2023-03-16 14:26:55 +0500

    full functionality of searching users by username v1

--- src/main/java/com/noobs/gazonuz/controllers/PitchPostController.java
+++ src/main/java/com/noobs/gazonuz/controllers/PitchPostController.java
@@ -2,15 +2,13 @@ package com.noobs.gazonuz.controllers;
 
 import com.noobs.gazonuz.domains.Pitch;
 import com.noobs.gazonuz.handler.CustomRuntimeException;
-import com.noobs.gazonuz.repositories.PitchRepo;
+import com.noobs.gazonuz.repositories.pitch.PitchRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
@@ -20,15 +18,16 @@ import java.util.Optional;
 public class PitchPostController {
 
 
-    private final PitchRepo pitchRepo;
-    @GetMapping("/{id}")
-    public String showPitchInfo(@PathVariable( value = "id") String id, Model model) {
+    private final PitchRepository pitchRepo;
+
+    @GetMapping( "/{id}" )
+    public String showPitchInfo(@PathVariable( value = "id" ) String id , Model model) {
 
         Optional<Pitch> pitch = pitchRepo.findById(id);
-        if(pitch.isPresent()){
-            model.addAttribute("pitch", pitch);
+        if ( pitch.isPresent() ) {
+            model.addAttribute("pitch" , pitch);
             return "pitch/PitchMainForCopy";
-        }else{
+        } else {
             throw new CustomRuntimeException("Page not found");
         }
 
