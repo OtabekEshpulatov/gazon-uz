aa4106fa4796f971fc6e5464fa06a45600e47d10
Javokhir544 <javokhir544@gmail.com>
2023-03-20 19:24:43 +0500

    select regions  but not view districts  in pitch  full

--- src/main/java/com/noobs/gazonuz/services/PitchService.java
+++ src/main/java/com/noobs/gazonuz/services/PitchService.java
@@ -1,6 +1,7 @@
 package com.noobs.gazonuz.services;
 
 
+import com.noobs.gazonuz.configs.properties.ApplicationProperties;
 import com.noobs.gazonuz.domains.Document;
 import com.noobs.gazonuz.domains.Pitch;
 import com.noobs.gazonuz.domains.auth.User;
@@ -21,6 +22,7 @@ import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -29,6 +31,9 @@ public class PitchService {
     private final PitchRepository pitchRepository;
     private final AddressService addressService;
     private final DocumentService documentService;
+    private final EmailService emailService;
+
+    private final ApplicationProperties properties;
 
 
     public boolean savePitch(PitchCreateDTO dto, User user) {
@@ -113,5 +118,42 @@ public class PitchService {
         }
         pitchOrderTimeDTO.setMessage(result);
         return pitchOrderTimeDTO;
+    }
+    public void updateStatus(String id , PitchStatus status) {
+
+
+        final Optional<Pitch> pitchOptional = pitchRepository.findById(id);
+
+
+        pitchOptional.ifPresent(pitch -> {
+
+
+            final User user = pitch.getUser();
+            final String email = user.getEmail();
+
+            if ( user.isEmailNotificationsAllowed() ) {
+                switch ( status ) {
+                    case BLOCKED -> {
+                        final String messageBody = properties.getProperties().getProperty("pitch.status.blocked.message.body").formatted(pitch.getCreatedAt());
+                        final String messageHeader = properties.getProperties().getProperty("pitch.status.blocked.message.header");
+                        emailService.sendMessageToEmailThroughSMTP(email , messageBody , messageHeader);
+                    }
+                    case REJECTED -> {
+                        final String messageBody = properties.getProperties().getProperty("pitch.status.rejected.message.body").formatted(pitch.getCreatedAt());
+                        final String messageHeader = properties.getProperties().getProperty("pitch.status.rejected.message.header");
+                        emailService.sendMessageToEmailThroughSMTP(email , messageBody , messageHeader);
+                    }
+                    case ACTIVE -> {
+                        final String messageBody = properties.getProperties().getProperty("pitch.status.active.message.body").formatted(pitch.getCreatedAt());
+                        final String messageHeader = properties.getProperties().getProperty("pitch.status.active.message.header");
+                        emailService.sendMessageToEmailThroughSMTP(email , messageBody , messageHeader);
+                    }
+                }
+            }
+
+            pitchRepository.updateStatusById(status , id);
+
+        });
+
     }
 }
