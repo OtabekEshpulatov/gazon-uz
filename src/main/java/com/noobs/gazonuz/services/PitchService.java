cb3076ca3a0ca21f666794237603a003c403edae
nodir <himmatovnodir1512@gmail.com>
2023-03-20 10:50:31 +0500

    carousel upgrade v2

--- src/main/java/com/noobs/gazonuz/services/PitchService.java
+++ src/main/java/com/noobs/gazonuz/services/PitchService.java
@@ -6,6 +6,7 @@ import com.noobs.gazonuz.domains.Pitch;
 import com.noobs.gazonuz.domains.auth.User;
 import com.noobs.gazonuz.domains.location.District;
 import com.noobs.gazonuz.dtos.PitchCreateDTO;
+import com.noobs.gazonuz.dtos.PitchOrderTimeDTO;
 import com.noobs.gazonuz.dtos.upload.DocumentCreateDTO;
 import com.noobs.gazonuz.enums.PitchStatus;
 import com.noobs.gazonuz.repositories.pitch.PitchRepository;
@@ -13,7 +14,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -84,5 +87,31 @@ public class PitchService {
             default:
                 return "th";
         }
+    }
+    public static PitchOrderTimeDTO findDateInterval(long i, long k){
+        String result = "";
+        i=i-1;
+        PitchOrderTimeDTO pitchOrderTimeDTO = new PitchOrderTimeDTO();
+        LocalDateTime currentDateTime = LocalDateTime.now(ZoneId.of("Asia/Tashkent"));
+        LocalDate localDate = LocalDate.now(ZoneId.of("Asia/Tashkent"));
+        LocalTime localTime = LocalTime.MIDNIGHT;
+        LocalDateTime currentDate = LocalDateTime.of(localDate,localTime);
+        if(currentDate.plusDays(k-1).plusHours(i).isAfter(currentDateTime)){
+            if(i<10){
+                result+="0"+i+":00-";
+            }else {
+                result+=i+":00-";
+            }
+            if(i+1>=10){
+                result+=i+1+":00";
+            }else {
+                result+="0"+(i+1)+":00";
+            }
+            pitchOrderTimeDTO.setIsAvl(true);
+        }else {
+            pitchOrderTimeDTO.setIsAvl(false);
+        }
+        pitchOrderTimeDTO.setMessage(result);
+        return pitchOrderTimeDTO;
     }
 }
